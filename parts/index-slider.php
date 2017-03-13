1c89554ccf16d6d197f3650a4e662815cd2b61da
Tu Nguyen <nvminhtu@gmail.com>
2017-03-13 15:25:19 +0700

    update shortcode to show in slider

--- parts/index-slider.php
+++ parts/index-slider.php
@@ -2,7 +2,6 @@
     <div class="inner clearfix">
         <?php 
           $shortcode_slider = get_field( 'homepage_slider','option');
-         // echo $shortcode_slider = "'".$shortcode_slider."'";
           echo do_shortcode($shortcode_slider);
         ?>
       </div>
