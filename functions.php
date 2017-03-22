f3079875291f3590b48914c344569ad285fb498a
Tu Nguyen <nvminhtu@gmail.com>
2017-03-22 19:19:00 +0700

    update and fix admin wordpress bar

--- functions.php
+++ functions.php
@@ -159,20 +159,64 @@ function myplugin_customize_toolbar( $wp_admin_bar ){
     return;
   }
   $my_account = $wp_admin_bar->get_node( 'my-account' );
-  
   $wp_admin_bar->remove_node( 'wp-logo' );
-  $wp_admin_bar->remove_node( 'display-name' );
-  
-  if ($my_account ){
-    $wp_admin_bar->add_node( array(
-      'parent'    => 'user-actions',
-      'id'    => 'user-url',
-      'title'   => '<span class="user-url">' . __( 'My Website' ) . '</span>',
-      'href'    => esc_url( $user->user_url )
-    ) );
-  }
+  $wp_admin_bar->remove_node( 'user-info' );
 }
 add_action( 'admin_bar_menu', 'myplugin_customize_toolbar', 999 );
 
+// change first name and last name order
+function wp_admin_bar_my_custom_account_menu( $wp_admin_bar ) {
+  $user_id = get_current_user_id();
+  $current_user = wp_get_current_user();
+  $profile_url = get_edit_profile_url( $user_id );
+
+  if ( 0 != $user_id ) {
+    /* Add the "My Account" menu */
+    $avatar = get_avatar( $user_id, 28 );
+
+    $u_firstname = $current_user->user_firstname;
+    $u_lastname = $current_user->user_lastname;
+    $u_displayname = $u_lastname.' '.$u_firstname;
+
+    $howdy = sprintf( __('Welcome, %1$s'), $u_displayname );
+    $class = empty( $avatar ) ? '' : 'with-avatar';
+
+    $my_account = $wp_admin_bar->get_node( 'my-account' );
+
+    $wp_admin_bar->add_menu( array(
+          'id' => 'my-account',
+          'parent' => 'top-secondary',
+          'title' => $howdy . $avatar,
+          'href' => $profile_url,
+          'meta' => array(
+          'class' => $class,
+        ),
+      ) );
+
+    }
+}
+add_action( 'admin_bar_menu', 'wp_admin_bar_my_custom_account_menu', 11 );
+
+// update style admin wordpress
+
+add_action('admin_head', 'custom_admin_wp');
+function custom_admin_wp() {
+  echo '<style>
+    #wpadminbar #wp-admin-bar-my-account.with-avatar #wp-admin-bar-user-actions>li {
+      margin-left: 0px;
+    }
+  </style>';
+}
+
+// remove pages: wpcf7 and tools in editor
+function wpse26980_remove_tools() {
+  $user = wp_get_current_user();
+  if(!(current_user_can('administrator'))) {
+    remove_menu_page( 'tools.php' );
+    remove_menu_page( 'wpcf7' );
+    remove_menu_page( 'edit-comments.php' );
+  }
+}
+add_action( 'admin_menu', 'wpse26980_remove_tools', 99 );
 
 ?>
